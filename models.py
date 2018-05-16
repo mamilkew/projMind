d70623ded3a9dccde07a5b75284f7070254002ab
mamilkew <mui_milk@hotmail.com>
2018-05-16 17:01:29 +0700

    updated and migrated db for relationship of user table and post table

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -19,13 +20,11 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    pub_date = db.Column(db.DateTime, nullable=False,
-        default=datetime.utcnow)
-
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
-        nullable=False)
-    category = db.relationship('Category',
-        backref=db.backref('posts', lazy=True))
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('posts', lazy=True))
 
     def __repr__(self):
         return '<Post %r>' % self.title
